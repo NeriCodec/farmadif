80923a99ef03442a3e0e733a3418ea61391e972a
Alan Neri <alanneri10@gmail.com>
2017-04-09 09:43:56 -0500

    agregando el yield principal, para que se pueda hacer referencia, al contenedor y poder crear los templates separados

--- resources/views/templates/principal.blade.php
+++ resources/views/templates/principal.blade.php
@@ -307,8 +307,10 @@
                 <div class="col-lg-12">
                     <h1 class="page-header">TITULO</h1>
                 </div>
-                <!-- /.col-lg-12 -->
             </div>
+            
+            @yield('contenedor')
+
          </div>
 
         <!-- jQuery -->
