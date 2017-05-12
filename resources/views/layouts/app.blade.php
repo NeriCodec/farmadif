5a26026276468d8a11b6b23d9f44a14134487289
Alan Neri <alanneri10@gmail.com>
2017-05-12 00:02:44 -0500

    arreglando confusiones:

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,6 +119,11 @@
                                     Entrada medicamento</span>
                                 </a>
                             </li>
+                            <li>
+                                <a href="#">
+                                    Medicamento Requerido</span>
+                                </a>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
